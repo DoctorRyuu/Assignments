9ac8d93f4fa8ac297fff59082ae1c99f1898298f
Phillip Ivey <danielivey1986@gmail.com>
2022-03-25 04:54:44 -0400

    Git training Commit

---
+++ terminal-practice/dir-two/main.js
@@ -0,0 +1,17 @@
+function nanopractice(){
+var input = document.getElementById("input").value
+	console.log(input)
+	if (input === "Hello") {
+		alert("Hello World!");
+		console.log("They said Hello.")
+	}
+	if (input === "Hey") {
+		alert("Hey World!");
+		console.log("They said Hey.")
+	}
+	if (input === "") {
+		alert("You could say Hello. Or at least Hey.");
+		console.log("Rude")
+	}
+}
+document.getElementById("form").addEventListener("submit", nanopractice)

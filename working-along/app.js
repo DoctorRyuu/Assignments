4c2334ca9fe55045a063426ed731aa2714409f3d
Phillip Ivey <danielivey1986@gmail.com>
2022-03-30 22:04:57 -0400

    workingalong

--- working-along/app.js
+++ working-along/app.js
@@ -1,29 +1,11 @@
 
-var result = document.querySelectorAll("ul#usernames > li")
-console.log(Array.from(result))
+//var result = document.querySelectorAll("ul#usernames > li")
+//console.log(Array.from(result))
 
-var ryuu = document.getElementById("me")
-var sis = document.getElementById("sis")
-var pruitt = document.getElementById("pruitt")
-var jewell = document.getElementById("jewell")
+//var text = document.getElementById("header").textContent
+//document.getElementById("para").textContent = text
 
-var clan = {
-    ryuu:true,
-    sis:true,
-    pruitt:true,
-    jewell:true
-}
+//var header = document.getElementById("header")
+//console.log(typeof header.style)
+//header.style.color = "red"
 
-console.log([ryuu, sis, pruitt, jewell])
-
-for (i = 0; i < result.length; i++){
-    console.log(result[i])
-}
-
-var person = {name: "Phillip"}
-person.name = "Daniel"
-console.log(person)
-
-var header = document.getElementById("header").textContent
-    console.log(header)
-document.getElementById("para").textContent = header

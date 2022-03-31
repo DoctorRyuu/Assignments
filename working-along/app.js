093bf9362d37d00c7f51e3c80e3f4ac01051072c
Phillip Ivey <danielivey1986@gmail.com>
2022-03-30 22:59:13 -0400

    workingalong

--- working-along/app.js
+++ working-along/app.js
@@ -9,3 +9,26 @@
 //console.log(typeof header.style)
 //header.style.color = "red"
 
+//var className = document.querySelector("#header").className += " new-class"
+
+//document.querySelector("#header").classList.add("new-class")
+//document.querySelector("#header").classList.remove("another")
+//document.querySelector("#header").classList.toggle("another")
+//console.log(document.querySelector("#header").className)
+
+//var inputBox = document.getElementById("text-input")
+//console.log(inputBox.value)
+
+//var newLi = document.createElement("li")
+//newLi.textContent = "3"
+//var mylist = document.getElementById("usernames")
+//mylist.append(newLi)
+//var newP = document.createElement("p")
+//newP.textContent = "So here's the new paragraph after the list."
+//newP.style.textAlign = "center"
+//newP.style.fontSize = "30px"
+//document.body.append(newP)
+
+var myList = document.getElementById("usernames")
+myList.innerHTML += "<li>3</li>"
+document.body.innerHTML += "<p>So here's the new element.</p>"
